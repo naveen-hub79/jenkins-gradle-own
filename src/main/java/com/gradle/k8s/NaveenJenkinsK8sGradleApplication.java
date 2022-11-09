f9728d8260998dc4ac59e157b1c5ddaefc5c039b
016720 <016720@host.docker.internal>
2022-11-09 17:38:28 +0530

    First commit

---
+++ src/main/java/com/gradle/k8s/NaveenJenkinsK8sGradleApplication.java
@@ -0,0 +1,13 @@
+package com.gradle.k8s;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NaveenJenkinsK8sGradleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NaveenJenkinsK8sGradleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gradle/k8s/NaveenJenkinsK8sGradleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gradle.k8s;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NaveenJenkinsK8sGradleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
